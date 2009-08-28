3aabc7ee70a0ea1107fa285c2702859e2d55d7a3
andre.lotta86 <andre.lotta86@bcb50e44-5817-11de-bc7c-c9df4fa8b3c6>
2009-08-28 03:52:52 +0000

    finish

---
+++ trunk/ztl.c
@@ -0,0 +1,13 @@
+#include "permserver.h"
+
+//PILA DI INTERAZIONE tra worker e writer
+
+int main(int argc,char *argv[]){
+	
+	/* primo controllo validita argomenti*/
+	if(argc == 0) exit(EXIT_FAILURE);
+	
+	//CONTATTARE IL SERVER SULLA SOCKET
+	
+	
+}
