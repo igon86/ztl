6867b4ccbd6665868d14aca82826ac13ba374a08
andre.lotta86 <andre.lotta86@bcb50e44-5817-11de-bc7c-c9df4fa8b3c6>
2009-10-06 16:35:29 +0000

    aggiunto lo stack

---
+++ trunk/istack.c
@@ -0,0 +1,53 @@
+/** 
+\file istack.c
+\author	Andrea Lottarini
+\brief pila delle infrazioni
+ 
+Implementazione della pila di infrazioni utilizzata dal client ZTL
+
+Si dichiara che il contenuto di questo file e` in ogni sua parte opera
+originale dell'autore.
+ */
+
+#include "istack.h"
+
+
+int addInfrazione (infr_t ** l,const char* s){
+
+	infr_t* temp = *l;
+	*l =  malloc(sizeof(infr_t));
+	
+	strncpy((*l)->passaggio,s,LPASSAGGIO);
+	(*l)->passaggio[LPASSAGGIO] = '\0';
+
+	(*l)->next = temp;
+
+	return 0;
+}
+
+infr_t* estraiInfrazione (infr_t ** l){
+	infr_t* ret = *l;
+	
+	if (*l != NULL){
+		*l = (*l)->next;
+	}
+
+	return ret;
+}
+
+
+int size(infr_t *l){
+	if(l!=NULL){
+		return 1+size(l->next);
+	}
+	else{
+		return 0;
+	}
+}
+
+void freeStack(infr_t *l){
+	if (l!=NULL){
+ 		free(l->next);
+		free(l);
+	}
+}
